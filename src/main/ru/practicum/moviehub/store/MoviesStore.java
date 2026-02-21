b785f46ddb076fb1c51cd38f8fb356826ee340f7
Artem <a.ermizin.corp@yandex.ru>
2026-02-21 15:43:02 +0700

    Исправил оформление стиля кода №4

--- src/main/ru/practicum/moviehub/store/MoviesStore.java
+++ src/main/ru/practicum/moviehub/store/MoviesStore.java
@@ -15,7 +15,7 @@ public class MoviesStore {
     public static final int EARLIEST_FILM_YEAR = 1888;
     public static final int CURRENT_YEAR = LocalDate.now().getYear();
 
-    private HashMap<Integer,Movie> movieMap = new HashMap<>();
+    private HashMap<Integer, Movie> movieMap = new HashMap<>();
     private Set<Integer> usedIds = new HashSet<>();
     private Random random = new Random();
 
